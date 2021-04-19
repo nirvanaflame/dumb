9fc87e1c834957453b08079de39d5b740b4d77b0
Sergey Putik <sergey.putik@mail.ru>
2021-04-19 22:21:36 +0200

    [codewars] How to compare strings

---
+++ playground/src/test/java/com/sputik/playground/codewars/StringEndingTest.java
@@ -0,0 +1,46 @@
+package com.sputik.playground.codewars;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class StringEndingTest {
+
+  @Test
+  void shouldVerifyStringEnding() {
+    assertAll(() -> {
+      assertThat(stringContainsEnding("abc", "c")).isTrue();
+      assertThat(stringContainsEnding("abc", "bc")).isTrue();
+      assertThat(stringContainsEnding("abc", "abc")).isTrue();
+      assertThat(stringContainsEnding("cba", "b")).isFalse();
+      assertThat(stringContainsEnding("cba", "bc")).isFalse();
+      assertThat(stringContainsEnding("cb", "bca")).isFalse();
+    });
+  }
+
+  @Test
+  void howKataSupposedToBeSolved() {
+    assertThat(smartComparison("abc", "bc")).isTrue();
+    assertThat(smartComparison("adc", "calc")).isFalse();
+  }
+
+  private boolean smartComparison(String base, String ending) {
+    return base.endsWith(ending);
+  }
+
+  private boolean stringContainsEnding(String base, String ending) {
+    return base.length() >= ending.length() && compareIntStreams(
+        base.subSequence(base.length() - ending.length(), base.length()).chars(),
+        ending.chars()
+    );
+  }
+
+  private boolean compareIntStreams(IntStream s1, IntStream s2) {
+    Set<Integer> set = s2.boxed().collect(Collectors.toSet());
+    return s1.allMatch(set::contains);
+  }
+}
