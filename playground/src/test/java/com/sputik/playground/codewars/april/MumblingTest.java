29e23b0c9660e9630c504500b621fa8d266129c2
nirvanaflame <sergey.putik@mail.ru>
2021-05-01 15:07:12 +0200

    [codewars] Generic nightmare
    Failed kata as it seems to me impossible to cast an array to a generic
    type
    
    Signed-off-by: nirvanaflame <sergey.putik@mail.ru>

--- playground/src/test/java/com/sputik/playground/codewars/april/MumblingTest.java
+++ playground/src/test/java/com/sputik/playground/codewars/april/MumblingTest.java
@@ -9,29 +9,38 @@ import org.junit.jupiter.api.Test;
 
 class MumblingTest {
 
-  public static String cleverSolution(String s) {
-    return IntStream.range(0, s.length())
-        .mapToObj(i -> IntStream.range(0, i + 1)
-            .mapToObj(i1 -> i1 == 0 ? String.valueOf(s.charAt(i)).toUpperCase()
-                : String.valueOf(s.charAt(i)).toLowerCase())
-            .collect(Collectors.joining())
-        ).collect(Collectors.joining("-"));
-  }
-
   @Test
   void shouldReturn() {
     assertThat(mumble("ZpglnRxqenU"))
         .isEqualTo("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
   }
 
-  private String mumble(String str) {
+  String mumble(String str) {
     AtomicInteger count = new AtomicInteger();
-    String s = str.toLowerCase();
-    String reduce = s.chars()
+    String reduce = str.toLowerCase()
+        .chars()
         .mapToObj(Character::toString)
         .reduce("", (acc, each) -> acc + each.toUpperCase() + each.repeat(
             count.getAndIncrement()) + "-");
 
     return reduce.substring(0, reduce.length() - 1);
+  }
+
+  @Test
+  void shouldReturnClever() {
+    assertThat(cleverSolution("ZpglnRxqenU"))
+        .isEqualTo("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+  }
+
+  String cleverSolution(String s) {
+    return IntStream.range(0, s.length())
+        .mapToObj(i -> IntStream.range(0, i + 1)
+            .mapToObj(i1 -> i1 == 0 ? String.valueOf(s.charAt(i))
+                .toUpperCase()
+                : String.valueOf(s.charAt(i))
+                    .toLowerCase())
+            .collect(Collectors.joining())
+        )
+        .collect(Collectors.joining("-"));
   }
 }
