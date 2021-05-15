615d0b55ee3ce00b2041ec430c50aa6ce48ac495
nirvanaflame <sergey.putik@mail.ru>
2021-05-15 13:40:53 +0200

    Codewars JS katas

---
+++ playground/src/test/js/codewars/7/may/TwoToOne.js
@@ -0,0 +1,15 @@
+function longest(a, b) {
+  return (a + b)
+    .split('')
+    .filter((val, index, arr) => arr.indexOf(val) === index)
+    .sort()
+    .join('')
+}
+
+
+const longestOneLiner = (s1,s2) => [...new Set(s1 + s2)].sort().join('')
+
+console.log(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+
+console.log(longestOneLiner("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
