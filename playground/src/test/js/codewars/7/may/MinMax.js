17cd372714835916a149fd25db290f10299c13e4
nirvanaflame <sergey.putik@mail.ru>
2021-05-20 16:59:31 +0200

    [codewars] MinMax
    Math for sequence is overpowerfull.
    Turning list into sequence just by typing dots is madness

---
+++ playground/src/test/js/codewars/7/may/MinMax.js
@@ -0,0 +1,17 @@
+function minMax(arr) {
+  return [
+    arr.reduce((acc, each) => acc > each ? acc = each : acc),
+    arr.reduce((acc, each) => acc < each ? acc = each : acc)
+  ]
+}
+
+console.log(minMax([1, 2, 3, 4, 5]), [1, 5]);
+console.log(minMax([2334454, 5]), [5, 2334454]);
+
+
+
+function minMaxClever(arr) {
+  return [Math.min(...arr), Math.max(...arr)]
+}
+
+console.log(minMaxClever([-5, -2, 0, 1, 2, 3, 4, 5]), [-5, 5]);
