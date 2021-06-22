b992f2966b346c1d70ce3b4eb6a953a886be66e2
nirvanaflame <sergey.putik@mail.ru>
2021-06-22 22:08:11 +0200

    [aws] Initial project
    Generate project by AWS SAM and aws-toolkit plugin?
    
    I assume that plugin added menu entry in the new project model window
    
    I can use SAM as backend engine for ran, test and deploy code to AWS?
    
    Signed-off-by: nirvanaflame <sergey.putik@mail.ru>

---
+++ aws-tutorials/HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.junit.jupiter.api.Test;
+
+
+class AppTest {
+
+  @Test
+  void successfulResponse() {
+    App app = new App();
+    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+
+    String content = result.getBody();
+    assertNotNull(content);
+
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
