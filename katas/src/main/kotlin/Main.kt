a51b91752e66a1183907cdced2ebb37500d65471
nirvanaflame <sergey.putik@mail.ru>
2021-06-28 22:19:56 +0200

    [refactor] Update Gradle build
    Signed-off-by: nirvanaflame <sergey.putik@mail.ru>

---
+++ katas/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val s1 = scanner.nextLine()
+    val n = scanner.nextInt()
+    val s2 = scanner.next()
+
+    println(s1)
+    println(n)
+    println(s2)
+}
