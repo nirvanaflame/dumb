205df6478c74349481e2b0723dbf9fbea412a222
nirvanaflame <sergey.putik@mail.ru>
2021-07-02 15:11:02 +0200

    [refactor] Add kata discription

--- katas/src/test/js/codewars/7/june/DigitalRoot.js
+++ katas/src/test/js/codewars/7/june/DigitalRoot.js
@@ -9,6 +9,13 @@ console.log(assert.strictEqual(digitalRootMath(16), 7))
 console.log(assert.strictEqual(digitalRootMath(456), 6))
 
 
+/*
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n.
+If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
+The input will be a non-negative integer.
+*/
 
 function digitalRoot(n) {
   return (n < 0 || n < 10) ? n : digitalRoot(
