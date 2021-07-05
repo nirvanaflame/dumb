ccf66218d0a6f04b6b19161f43f6d8a18cbae6ca
nirvanaflame <sergey.putik@mail.ru>
2021-07-05 11:22:09 +0200

    [jenkins] Convert record to Class
    Jenkins supports java version 11 at max
    
    Signed-off-by: nirvanaflame <sergey.putik@mail.ru>

--- reactive-tutorials/src/main/java/com/playground/sputik/model/User.java
+++ reactive-tutorials/src/main/java/com/playground/sputik/model/User.java
@@ -1,5 +1,49 @@
 package com.playground.sputik.model;
 
-public record User(String username, String firstName, String lastName) {
+import java.util.Objects;
+
+public final class User {
+
+  private final String username;
+  private final String firstName;
+  private final String lastName;
+
+  public User(String username, String firstName, String lastName) {
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public String username() { return username; }
+
+  public String firstName() { return firstName; }
+
+  public String lastName() { return lastName; }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null || obj.getClass() != this.getClass()) {
+      return false;
+    }
+    var that = (User) obj;
+    return Objects.equals(this.username, that.username) && Objects.equals(this.firstName,
+                                                                          that.firstName) && Objects
+               .equals(this.lastName, that.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, firstName, lastName);
+  }
+
+  @Override
+  public String toString() {
+    return "User[" + "username=" + username + ", " + "firstName=" + firstName + ", " + "lastName="
+           + lastName + ']';
+  }
+
 
 }
