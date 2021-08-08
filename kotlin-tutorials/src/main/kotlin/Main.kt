bd24728aefa30a5f064c4734b8ba2fc25ac7cb0c
nirvanaflame <sergey.putik@mail.ru>
2021-08-08 17:22:47 +0200

    Learn Kotlin with function
    Signed-off-by: nirvanaflame <sergey.putik@mail.ru>

--- kotlin-tutorials/src/main/kotlin/Main.kt
+++ kotlin-tutorials/src/main/kotlin/Main.kt
@@ -1,9 +1,29 @@
-fun main(vararg args: String) {
+fun main() {
+
 
     println(getAge("34 asd"))
     println(getAgeTake1("34 asd"))
 
+    val (a, b, c) = List(3) { readLine()!!.first() }
+
+    print(
+            a.inc() == b && b.inc() == c
+    )
+
+    destructionExample()
+    destructionExampleWith()
+
+    with(Array(3) { readLine()!!.first() }) {
+        component1().inc() == component2() && component2().inc() == component3()
+    }.let(::println)
+
+    val n = 123
+    n.toString().reversed().let { print(it) }
+
 }
+
+fun destructionExample(): Unit = List(3) { readLine()!!.first() }.let { print(it[0].inc() == it[1] && it[1].inc() == it[2]) }
+fun destructionExampleWith(): Unit = print(with(List(3) { readLine()!!.first() }) { component1().inc() == component2() && component2().inc() == component2() })
 
 fun getAge(yearsOld: String) = yearsOld.takeWhile { it.isDigit() }
 fun getAgeTake1(yearsOld: String) = yearsOld.first()
--- spring-boot-jbehave/src/test/java/com/sputik/jbehave/TestRunner.java
+++ spring-boot-jbehave/src/test/java/com/sputik/jbehave/TestRunner.java
@@ -2,49 +2,80 @@ package com.sputik.jbehave;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.failures.FailingUponPendingStep;
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.embedder.EmbedderControls;
+import org.jbehave.core.failures.PassingUponPendingStep;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.model.ExamplesTableFactory;
+import org.jbehave.core.model.TableParsers;
+import org.jbehave.core.model.TableTransformers;
 import org.jbehave.core.reporters.CrossReference;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.PrintStreamStepdocReporter;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.ParameterControls;
 import org.jbehave.core.steps.ParameterConverters;
+import org.jbehave.core.steps.ParameterConverters.ExamplesTableConverter;
 import org.jbehave.core.steps.SilentStepMonitor;
 import org.jbehave.core.steps.spring.SpringStepsFactory;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class TestRunner
-    extends JUnitStories {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {ApplicationToTest.class})
+public class TestRunner extends JUnitStories {
 
-  @Autowired
-  private ApplicationContext applicationContext;
+  @Autowired private ApplicationContext applicationContext;
 
   public TestRunner() {
-    initJBehaveConfiguration();
+    Class<?> thisClass = this.getClass();
+    useConfiguration(new MostUsefulConfiguration().useStoryLoader(
+            new LoadFromClasspath(thisClass.getClassLoader()))
+                         .usePendingStepStrategy(new PassingUponPendingStep())
+                         .useStepdocReporter(new PrintStreamStepdocReporter())
+                         .useStoryReporterBuilder(getStoryReporterBuilder(thisClass))
+                         .useParameterConverters(getParameterConverters())
+                         .useStepMonitor(new SilentStepMonitor()));
   }
 
-  private void initJBehaveConfiguration() {
-    Class<?> thisClass = this.getClass();
-    useConfiguration(new MostUsefulConfiguration()
-            .useStoryLoader(new LoadFromClasspath(thisClass.getClassLoader()))
-            .usePendingStepStrategy(new FailingUponPendingStep())
-            .useStepdocReporter(new PrintStreamStepdocReporter())
-            .useStoryReporterBuilder(new StoryReporterBuilder()
-                .withCodeLocation(CodeLocations.codeLocationFromClass(thisClass))
-                .withFormats(Format.CONSOLE, Format.TXT, Format.HTML, Format.XML, Format.STATS)
-                .withCrossReference(new CrossReference())
-                .withFailureTrace(true))
-            .useParameterConverters(new ParameterConverters()
-                .addConverters(
-                    new ParameterConverters.DateConverter(new SimpleDateFormat("yyyy-MM-dd"))))
-            .useStepMonitor(new SilentStepMonitor())
-    );
+  @PostConstruct
+  public void initialize() {
+    Embedder embedder = new Embedder();
+    embedder.useEmbedderControls(embedderControls());
+    useEmbedder(embedder);
+  }
+
+  private EmbedderControls embedderControls() {
+    return new EmbedderControls().doIgnoreFailureInStories(true).doVerboseFailures(true)
+        .useStoryTimeouts("2000").useThreads(8);
+  }
+
+  private ExamplesTableFactory createExampleTableFactory() {
+    return new ExamplesTableFactory(resourcePath -> "sad", new ParameterConverters(),
+                                    new ParameterControls(), new TableParsers(),
+                                    new TableTransformers());
+  }
+
+  private ParameterConverters getParameterConverters() {
+    return new ParameterConverters().addConverters(
+        new ParameterConverters.DateConverter(new SimpleDateFormat("yyyy-MM-dd")),
+        new ExamplesTableConverter(createExampleTableFactory()));
+  }
+
+  private StoryReporterBuilder getStoryReporterBuilder(Class<?> thisClass) {
+    return new StoryReporterBuilder().withCodeLocation(
+            CodeLocations.codeLocationFromClass(thisClass))
+        .withFormats(Format.CONSOLE, Format.TXT, Format.HTML, Format.XML, Format.STATS)
+        .withCrossReference(new CrossReference()).withFailureTrace(true);
   }
 
   @Override
@@ -53,9 +84,8 @@ public class TestRunner
   }
 
   protected List<String> storyPaths() {
-    return new StoryFinder()
-        .findPaths(CodeLocations.codeLocationFromClass(this.getClass()), "**/*.story",
-            "**/excluded*.story");
+    return new StoryFinder().findPaths(CodeLocations.codeLocationFromClass(this.getClass()),
+                                       "**/Test.story", "**/excluded*.story");
   }
 
 }
